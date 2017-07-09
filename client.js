f4a85f792b6a5d497e69098ef18279af745080a9
aafrey <aafrey85@gmail.com>
2017-07-09 14:49:40 -0400

    updated client.js to use net rather than http

--- client.js
+++ client.js
@@ -1,6 +1,16 @@
-const { pull, fetch } = require('./helper')
+const { pull, connect, write, take, map } = require('./helper')
+
+const stream = connect(9999, process.argv[2])
 
 pull(
-  fetch.result('http://127.0.0.1:9999'),
+  stream,
+  take(1),
+  pull.map((data) => data.toString('utf8')),
   pull.log()
+  //write('./little.file', {}, function (
+  //  err
+  //){
+  //  if (err) console.log(err)
+  //  else console.log('all done')
+  //})
 )
